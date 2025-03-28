91e70c38bbddca67af66ad3f8760597df7085410
marcorigiroli <mrigiroli@mail.austral.edu.ar>
2025-03-28 12:29:47 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,7 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    print(first_name.lower(), last_name.lower())   # ada lovelace
+    print(first_name.capitalize(), last_name.capitalize())   # Ada Lovelace
+    print(first_name.upper(), last_name.upper())   # ADA LOVELACE
+    print("\t" + first_name.lower(), last_name.lower())   # "\t" para la sangria 
